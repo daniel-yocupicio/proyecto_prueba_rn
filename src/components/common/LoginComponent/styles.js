ea15c899f6cec8c005bba364323a2353070080fc
daniel-yocupicio <dag_98@hotmail.com>
2022-05-18 19:16:35 -0500

    fix codeclimate problems

--- src/components/common/LoginComponent/styles.js
+++ src/components/common/LoginComponent/styles.js
@@ -3,28 +3,12 @@ const stylesLoginComponent = StyleSheet.create({
   container: {
     position: 'absolute',
     width: Dimensions.get('screen').width - 30,
-    height: 400,
+    height: 300,
     top: 70,
     right: 15,
     borderColor: '#EAEBEC',
     borderRadius: 20,
     borderWidth: 1,
-  },
-  card: {borderRadius: 7, opacity: 0.95},
-  cardtitle: {fontSize: 15, alignItems: 'center'},
-  input1: {fontSize: 18, marginBottom: -10, borderRadius: 10},
-  input2: {fontSize: 18, marginBottom: -10, marginTop: -10},
-  cardtitle2: {
-    fontSize: 15,
-    alignItems: 'center',
-    marginVertical: -10,
-    marginBottom: 3,
-  },
-  inputcontainer: {borderRadius: 10, backgroundColor: '#fff'},
-  touchabletext: {
-    width: Dimensions.get('screen').width,
-    marginTop: 20,
-    marginLeft: 15,
   },
 });
 
