77362aec73d48468e385f32dc1a584c178f8ca7b
daniel-yocupicio <dag_98@hotmail.com>
2022-05-20 00:02:57 -0500

    fix form data

--- App.js
+++ App.js
@@ -5,12 +5,11 @@
  * @format
  * @flow strict-local
  */
- import 'react-native-gesture-handler';
- import React from 'react';
- import { NavigationContainer } from '@react-navigation/native';
- import Routes from './src/routes';
- import { IsLogedContextProvider } from './src/context/IsLogedContext';
-
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import Routes from './src/routes';
+import {IsLogedContextProvider} from './src/context/IsLogedContext';
 
 const App = () => {
   return (
--- src/components/common/InputsLoginRegisterComponent.js
+++ src/components/common/InputsLoginRegisterComponent.js
@@ -1,29 +1,83 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import {Input, Button, Card} from 'react-native-elements';
 import styles from '../globalstyles';
 
+function InputsForm({onChange, type}) {
+  return (
+    <View>
+      <Input
+        placeholder="correo"
+        style={styles.stylesInputsComponentsstyle.input1}
+        onChange={(e, a) => onChange(e, 'email')}
+      />
+      <Input
+        placeholder="contraseña"
+        style={styles.stylesInputsComponentsstyle.input2}
+        onChange={(e, a) => onChange(e, 'password')}
+      />
+      {type === 'Iniciar sesión' ? null : (
+        <Input
+          placeholder="repita la contraseña"
+          style={styles.stylesInputsComponentsstyle.input2}
+          onChange={(e, a) => onChange(e, 'repeatPassword')}
+        />
+      )}
+    </View>
+  );
+}
+
 export default function InputsLoginRegisterComponent({type}) {
+  const [data, setData] = useState(defaultValue());
+  const [warningData, setwarningData] = useState(false);
+
+  const onChange = (e, key) => {
+    setData({...data, [key]: e.nativeEvent.text});
+  };
+
   return (
     <Card containerStyle={styles.stylesInputsComponentsstyle.card}>
       <Card.Title style={styles.stylesInputsComponentsstyle.cardtitle}>
         {type}
       </Card.Title>
-      <Input
-        containerStyle={styles.stylesInputsComponentsstyle.inputcontainer}
-        placeholder="correo"
-        style={styles.stylesInputsComponentsstyle.input1}
+      <InputsForm onChange={onChange} type={type} />
+      <Button
+        title={type}
+        onPress={(d, s, w) => validateData(data, setwarningData, warningData)}
       />
-      <Input
-        placeholder="contraseña"
-        style={styles.stylesInputsComponentsstyle.input2}
-      />
-      {type === 'Registrar cuenta' ? (
-        <Input
-          placeholder="repita la contraseña"
-          style={styles.stylesInputsComponentsstyle.input2}
-        />
-      ) : null}
-      <Button title={type} />
     </Card>
   );
 }
+
+function defaultValue() {
+  return {
+    email: '',
+    password: '',
+    repeatPassword: '',
+  };
+}
+
+const validateData = (datos, warningFunction, warningData) => {
+  if (datos.password === '' || datos.password.length < 6) {
+    warningFunction(true);
+  } else {
+    warningFunction(false);
+  }
+  if (datos.password !== datos.repeatPassword) {
+    warningFunction(true);
+  } else {
+    warningFunction(false);
+  }
+  if (
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(
+      datos.email,
+    )
+  ) {
+    warningFunction(false);
+  } else {
+    warningFunction(true);
+  }
+  if (warningData) {
+    //submit();
+  }
+};
