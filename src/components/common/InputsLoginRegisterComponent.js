1867fa5a4744161e1ca2faacc022c01ca512e309
daniel-yocupicio <dag_98@hotmail.com>
2022-05-20 01:33:38 -0500

    add firebase auth with email

--- src/components/common/InputsLoginRegisterComponent.js
+++ src/components/common/InputsLoginRegisterComponent.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
-import {Input, Button, Card} from 'react-native-elements';
+import {View, Text, Dimensions} from 'react-native';
+import {Input, Button, Card, Overlay} from 'react-native-elements';
 import styles from '../globalstyles';
+import auth from '@react-native-firebase/auth';
 
 function InputsForm({onChange, type}) {
   return (
@@ -27,13 +28,28 @@ function InputsForm({onChange, type}) {
   );
 }
 
+function Error({isvisible, warningChange}) {
+  return (
+    <Overlay isVisible={isvisible} onBackdropPress={() => warningChange(false)}>
+      <View
+        style={{
+          width: Dimensions.get('screen').width - 100,
+          alignItems: 'center',
+        }}>
+        <Text style={{fontSize: 20, color: '#CB4335', fontWeight: 'bold'}}>
+          Error
+        </Text>
+      </View>
+    </Overlay>
+  );
+}
+
 export default function InputsLoginRegisterComponent({type}) {
   const [data, setData] = useState(defaultValue());
-  const [warningData, setwarningData] = useState(false);
+  const [warningData, setwarningData] = useState(true);
 
-  const onChange = (e, key) => {
-    setData({...data, [key]: e.nativeEvent.text});
-  };
+  const onChange = (e, key) => setData({...data, [key]: e.nativeEvent.text});
+  const warningChange = value => setwarningData(value);
 
   return (
     <Card containerStyle={styles.stylesInputsComponentsstyle.card}>
@@ -45,6 +61,7 @@ export default function InputsLoginRegisterComponent({type}) {
         title={type}
         onPress={(d, s, w) => validateData(data, setwarningData, warningData)}
       />
+      <Error isvisible={warningData} warningChange={warningChange} />
     </Card>
   );
 }
@@ -61,23 +78,36 @@ const validateData = (datos, warningFunction, warningData) => {
   if (datos.password === '' || datos.password.length < 6) {
     warningFunction(true);
   } else {
-    warningFunction(false);
-  }
-  if (datos.password !== datos.repeatPassword) {
-    warningFunction(true);
-  } else {
-    warningFunction(false);
-  }
-  if (
-    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(
-      datos.email,
-    )
-  ) {
-    warningFunction(false);
-  } else {
-    warningFunction(true);
-  }
-  if (warningData) {
-    //submit();
+    if (
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(
+        datos.email,
+      )
+    ) {
+      if (datos.password === datos.repeatPassword) {
+        warningFunction(false);
+        createAccount(datos.email, datos.password);
+      } else {
+        warningFunction(true);
+      }
+    } else {
+      warningFunction(true);
+    }
   }
 };
+
+function createAccount(email, password) {
+  auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      console.log('User account created & signed in!');
+    })
+    .catch(error => {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('That email address is already in use!');
+      }
+      if (error.code === 'auth/invalid-email') {
+        console.log('That email address is invalid!');
+      }
+      console.error(error);
+    });
+}
